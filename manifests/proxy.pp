ea608de6957f12782fcb751555c83c44aecf3249
Stefan Goethals <stefan@zipkid.eu>
2013-05-03 13:33:32 +0200

    real erb

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,10 +1,19 @@
 class puppetbootstrap::proxy ( $host, $port = 80, $user = undef, $pass = undef ) {
+
   file { '/etc/profile.d/proxy.sh':
     ensure  => present,
     content => "export http_proxy=http://${host}:${port}\nexport https_proxy=http://${host}:${port}",
   }
   file { '/etc/yum.conf':
-    ensure => present,
-    source => 'puppet:///modules/puppetbootstrap/yum.conf',
+    ensure  => present,
+    content => template("puppetbootstrap/yum.conf.erb"),
+    #source  => 'puppet:///modules/puppetbootstrap/yum.conf',
   }
+  exec { "export http_proxy=http://${host}:${port}": 
+    provider => shell,
+  }
+  exec { "export https_proxy=http://${host}:${port}":
+    provider => shell,
+  }
+
 }
