b2fe715200c57a58767a63f0ed152764f5a40dee
Stefan Goethals <stefan@zipkid.eu>
2013-05-03 09:59:11 +0200

    More proxy….

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,7 +1,7 @@
 class puppetbootstrap::proxy ( $host, $port = 80, $user = undef, $pass = undef ) {
   file { '/etc/profile.d/proxy.sh':
     ensure  => present,
-    content => "export http_proxy=http://${host}:${port}",
+    content => "export http_proxy=http://${host}:${port}\nexport https_proxy=http://${host}:${port}",
   }
   file { '/etc/yum.conf':
     ensure => present,
