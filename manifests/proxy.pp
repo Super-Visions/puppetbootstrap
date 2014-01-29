c833cc73fb484834391d451b9254c9c29b9f64c0
Stefan Goethals <stefan@zipkid.eu>
2014-01-29 11:27:24 +0100

    remove yum.conf mgmnt

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -4,11 +4,11 @@ class puppetbootstrap::proxy ( $host, $port = 80, $user = undef, $pass = undef )
     ensure  => present,
     content => "export http_proxy=http://${host}:${port}\nexport https_proxy=http://${host}:${port}",
   }
-  file { '/etc/yum.conf':
-    ensure  => present,
-    content => template("puppetbootstrap/yum.conf.erb"),
-    #source  => 'puppet:///modules/puppetbootstrap/yum.conf',
-  }
+  #  file { '/etc/yum.conf':
+  #  ensure  => present,
+  #  content => template("puppetbootstrap/yum.conf.erb"),
+  #  #source  => 'puppet:///modules/puppetbootstrap/yum.conf',
+  #}
   exec { "export http_proxy=http://${host}:${port}": 
     provider => shell,
   }
