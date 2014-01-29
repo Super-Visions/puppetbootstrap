89087f00ffe88e8dd932cacc8892dc9f4253c1c5
Stefan Goethals <stefan@zipkid.eu>
2014-01-29 23:21:46 +0100

    export...

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -5,26 +5,26 @@ class puppetbootstrap::proxy ( $host, $port = 80, $proto = 'http', $user = undef
     content => "export http_proxy=${proto}://${host}:${port}\nexport https_proxy=${proto}://${host}:${port}\nexport no_proxy=${noproxy}",
   }
 
-#  if $ensure == 'present' {
-#    $proxy_action = 'export'
-#  } else {
-#    $proxy_action = 'unset'
-#  }
-#  exec { "${proxy_action} http_proxy=${proto}://${host}:${port}": 
-#    provider => shell,
-#  }
-#  exec { "${proxy_action} https_proxy=${proto}://${host}:${port}":
-#    provider => shell,
-#  }
-#
-#  # export export no_proxy=.mobistar.be
-#  if $noproxy {
-#    $noproxy_action = 'export'
-#  } else {
-#    $noproxy_action = 'unset'
-#  }
-#  exec { "${noproxy_action} no_proxy=${noproxy}": 
-#    provider => shell,
-#  }
+  if $ensure == 'present' {
+    $proxy_action = 'export'
+  } else {
+    $proxy_action = 'unset'
+  }
+  exec { "${proxy_action} http_proxy=${proto}://${host}:${port}": 
+    provider => shell,
+  }
+  exec { "${proxy_action} https_proxy=${proto}://${host}:${port}":
+    provider => shell,
+  }
+
+  # export export no_proxy=.mobistar.be
+  if $noproxy {
+    $noproxy_action = 'export'
+  } else {
+    $noproxy_action = 'unset'
+  }
+  exec { "${noproxy_action} no_proxy=${noproxy}": 
+    provider => shell,
+  }
 
 }
