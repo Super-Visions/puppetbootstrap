312fda450496197b9e49167225f2dc3306f262aa
Christophe Fonteyne <christophe.fonteyne@super-visions.com>
2014-03-20 17:22:24 +0100

    remove useless execs

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,11 +1,11 @@
-class puppetbootstrap::proxy ( 
-  $host, 
-  $port = 80, 
-  $proto = 'http', 
-  $user = undef, 
-  $pass = undef, 
-  $ensure = 'present', 
-  $noproxy = undef 
+class puppetbootstrap::proxy (
+  $host,
+  $port = 80,
+  $proto = 'http',
+  $user = undef,
+  $pass = undef,
+  $ensure = 'present',
+  $noproxy = undef
 ) {
 
   file { '/etc/profile.d/proxy.sh':
@@ -14,14 +14,14 @@ class puppetbootstrap::proxy (
   }
 
   if $ensure == 'present' {
-    $proxy_action = 'export'
-    $augeas_action = 'set'
-    exec { "${proxy_action} http_proxy=${proto}://${host}:${port}": 
-      provider => shell,
-    }
-    exec { "${proxy_action} https_proxy=${proto}://${host}:${port}":
-      provider => shell,
-    }
+    // $proxy_action = 'export'
+    // $augeas_action = 'set'
+    // exec { "${proxy_action} http_proxy=${proto}://${host}:${port}":
+    //   provider => shell,
+    // }
+    // exec { "${proxy_action} https_proxy=${proto}://${host}:${port}":
+    //   provider => shell,
+    // }
   } else {
     $proxy_action = 'unset'
     $augeas_action = 'rm'
@@ -44,10 +44,10 @@ class puppetbootstrap::proxy (
 
   # export export no_proxy=.mobistar.be
   if $noproxy {
-    $noproxy_action = 'export'
-    exec { "${noproxy_action} no_proxy=${noproxy}": 
-      provider => shell,
-    }
+    // $noproxy_action = 'export'
+    // exec { "${noproxy_action} no_proxy=${noproxy}": 
+    //   provider => shell,
+    // }
   } else {
     $noproxy_action = 'unset'
   }
